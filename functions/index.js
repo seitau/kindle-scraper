b30c9bd11cc1c407a54815dec296d79396258c97
Seita Uchimura <c201552k@gmail.com>
2018-11-18 13:34:12 -0800

    evernote oauth実装

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,31 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const serviceAccount = require('../service_account/kindle-7ef16-firebase-adminsdk-ksy3r-3fde325273.json');
+const settings = {timestampsInSnapshots: true};
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+admin.firestore().settings(settings);
+const db = admin.firestore()
+
+exports.evernoteAuth = functions.https.onRequest((req, res) => {
+  const params = req.query;
+  if (!params.hasOwnProperty('oauth_token')) {
+    params['oauth_token'] = 'not provided';
+  }
+  return db.collection('auth').doc('evernote')
+    .update({
+      oauth_token: params['oauth_token'],
+      oauth_verifier: params['oauth_verifier'],
+      sandbox_lnb: params['sandbox_lnb']
+    })
+    .then(() => {
+      console.log("Document written!: ", req.query);
+      return res.send("Evernote auth successfully executed");
+    })
+    .catch((err) => {
+      console.error("Error adding document: ", err);
+      return res.send("Error adding document: ", err);
+    });
+});
