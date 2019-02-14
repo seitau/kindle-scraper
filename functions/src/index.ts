9756aa3b633338525e775cc0ad47f0f62dd5e48a
Seita Uchimura <c201552k@gmail.com>
2019-02-13 18:49:33 -0800

    :wrench: Fix:update tsconfig

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -52,37 +52,38 @@ exports.ogp = firebase.functions.https.onRequest((req, res) => {
         });
 });
 
-exports.scrapeKindle = firebase.functions.https.onRequest(async (req, res) => {
-    if (!/application\/json/g.test(req.get('content-type'))) {
-        console.error("Error scraping kindle: request has to be application/json format");
-        return res.status(400).json({ error: "Error scraping kindle: request has to be application/json format" });
+exports.scrapeKindle = firebase.functions
+    .runWith({ memory: '1GB' }).https.onRequest(async (req, res) => {
+        if (!/application\/json/g.test(req.get('content-type'))) {
+            console.error("Error scraping kindle: request has to be application/json format");
+            return res.status(400).json({ error: "Error scraping kindle: request has to be application/json format" });
 
-    } else if (req.method !== "POST") {
-        console.error("Error scraping kindle: request has to be post method");
-        return res.status(400).json({ error: "Error scraping kindle: request has to be post method" });
+        } else if (req.method !== "POST") {
+            console.error("Error scraping kindle: request has to be post method");
+            return res.status(400).json({ error: "Error scraping kindle: request has to be post method" });
 
-    }
+        }
 
-    const KindleScraper = require('./common/kindle-scraper').default;
-    const puppeteer = require('puppeteer');
-    const body = req.body;
-    if (!body.hasOwnProperty('email') || !body.hasOwnProperty('password')) {
-        console.error("Error scraping kindle: please provide email and password");
-        return res.status(400).json({ error: "Error scraping kindle: please provide email and password" });
-    }
-    const browser = await puppeteer.launch({
-        headless: true,
-    });
-    const page = await browser.newPage()
-    const scraper = new KindleScraper(browser, page, body.email, body.password);
-    await scraper.scrapeKindle()
-        .catch((err) => {
-            console.error(err);
-            return res.status(500).json({ error: "Error scraping kindle: " + err });
+        const KindleScraper = require('./common/kindle-scraper').default;
+        const puppeteer = require('puppeteer');
+        const body = req.body;
+        if (!body.hasOwnProperty('email') || !body.hasOwnProperty('password')) {
+            console.error("Error scraping kindle: please provide email and password");
+            return res.status(400).json({ error: "Error scraping kindle: please provide email and password" });
+        }
+        const browser = await puppeteer.launch({
+            headless: true,
         });
-    return res.status(200).json({
-        result: "Successfully scraped kindle",
+        const page = await browser.newPage()
+        const scraper = new KindleScraper(browser, page, body.email, body.password);
+        await scraper.scrapeKindle()
+            .catch((err) => {
+                console.error(err);
+                return res.status(500).json({ error: "Error scraping kindle: " + err });
+            });
+        return res.status(200).json({
+            result: "Successfully scraped kindle",
 
+        });
     });
-});
 
