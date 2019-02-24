d114be8d5f2f0969265d969e007f469f5747a4a5
Seita Uchimura <c201552k@gmail.com>
2019-02-24 15:54:25 -0800

    :watermelon: Add:thread classを実装

--- dist/js/sketch.js
+++ dist/js/sketch.js
@@ -1,87 +1,61 @@
 class Thread {
-    constructor() {
-        this.xspacing = 16;
-        this.w;
-        this.theta = 0.0;
-        this.amplitude = 75.0;
-        this.period = 400.0;
-        this.dx; 
-        this.yvalues;
+    constructor(xspacing, theta, angularVelocity, amplitude, period, color) {
+        this.xspacing = xspacing;
+        this.width = windowWidth;
+        this.theta = theta;
+        this.angularVelocity = angularVelocity;
+        this.amplitude = amplitude;
+        this.period = period;
+        this.dx = (TWO_PI / this.period) * this.xspacing 
+        this.yvalues = new Array(floor(this.width / this.xspacing));
+        this.color = color;
+    }
+
+    calculateWave() {
+        this.theta += this.angularVelocity;
+
+        let x = this.theta;
+        for (let i = 0; i < this.yvalues.length; i++) {
+            this.yvalues[i] = sin(x) * this.amplitude;
+            x += this.dx;
+        }
     }
 
     render() {
-
+        this.calculateWave();
+        noStroke();
+        fill(this.color);
+        for (let x = 0; x < this.yvalues.length; x++) {
+            ellipse(x * this.xspacing, windowHeight / 2 + this.yvalues[x], 16, 16);
+        }
     }
-
 }
 
-let xspacing = 16; // Distance between each horizontal location
-let w; // Width of entire wave
-let theta1 = 0.0; // Start angle at 0
-let theta2 = 0.0; // Start angle at 0
-let amplitude = 75.0; // Height of wave
-let period = 400.0; // How many pixels before the wave repeats
-let dx; // Value for incrementing x
-let yvalues1; // Using an array to store height values for the wave
-let yvalues2; // Using an array to store height values for the wave
-
+let thread;
+let thread_2;
+let thread_3;
+let thread_4;
+let thread_5;
+let thread_6;
 function setup() {
     createCanvas(windowWidth, windowHeight);
+    thread = new Thread(7, 10, 0.04, 75.0, 200.0, 'yellow');
+    thread_2 = new Thread(10, 0.0, 0.07, 100.0, 500.0, 'blue');
+    thread_3 = new Thread(10, 3.0, 0.02, 140.0, 500.0, 'red');
+    thread_4 = new Thread(10, 6.0, 0.04, 160.0, 500.0, 'green');
+    thread_5 = new Thread(10, 1.0, 0.08, 300.0, 500.0, 'pink');
+    thread_6 = new Thread(10, 8.0, 0.01, 40.0, 500.0, 'gray');
     background(0);
-    w = width + 16;
-    dx = (TWO_PI / period) * xspacing;
-    yvalues1 = new Array(floor(w / xspacing));
-    yvalues2 = new Array(floor(w / xspacing));
 }
 
 function draw() {
     background(0);
-    renderWave1();
-    renderWave2();
-}
-
-function calcWave1() {
-    // Increment theta (try different values for
-    // 'angular velocity' here)
-    theta1 += 0.04;
-
-    // For every x value, calculate a y value with sine function
-    let x = theta1;
-    for (let i = 0; i < yvalues2.length; i++) {
-        yvalues1[i] = sin(x) * amplitude;
-        x += dx;
-    }
-}
-function calcWave2() {
-    // Increment theta (try different values for
-    // 'angular velocity' here)
-    theta2 += 0.01;
-
-    // For every x value, calculate a y value with sine function
-    let x = theta2;
-    for (let i = 0; i < yvalues2.length; i++) {
-        yvalues2[i] = sin(x) * amplitude;
-        //x += dx;
-    }
-}
-
-function renderWave1() {
-    calcWave1();
-    noStroke();
-    fill(255);
-    // A simple way to draw the wave with an ellipse at each location
-    for (let x = 0; x < yvalues1.length; x++) {
-        ellipse(x * xspacing, height / 2 + yvalues1[x], 16, 16);
-    }
-}
-function renderWave2() {
-    calcWave2();
-    noStroke();
-    fill(255);
-    // A simple way to draw the wave with an ellipse at each location
-    for (let x = 0; x < yvalues2.length; x++) {
-        ellipse(x * xspacing, height / 2 + yvalues2[x], 16, 16);
-    }
+    thread.render()
+    thread_2.render()
+    thread_3.render()
+    thread_4.render()
+    thread_5.render()
+    thread_6.render()
 }
 
 function windowResized() {
