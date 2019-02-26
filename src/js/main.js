4d46fafe4110df7ad79661126dabbac3927219f7
Seita Uchimura <c201552k@gmail.com>
2019-02-26 13:58:39 -0800

    :+1: Update:p5をnpm moduleに乗り換え

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,16 @@
+import P5 from 'p5'
+
 class Thread {
-    constructor(param) {
+    constructor(p5, param) {
+        this.p5 = p5;
         this.xspacing = param.xspacing;
-        this.width = windowWidth;
+        this.width = p5.windowWidth;
         this.theta = param.theta;
         this.angularVelocity = param.angularVelocity;
         this.amplitude = param.amplitude;
         this.period = param.period;
-        this.dx = (TWO_PI / this.period) * this.xspacing 
-        this.yvalues = new Array(floor(this.width / this.xspacing));
+        this.dx = (p5.TWO_PI / this.period) * this.xspacing 
+        this.yvalues = new Array(p5.floor(this.width / this.xspacing));
         this.color = param.color;
         this.circles = new Array();
         this.radius = 16;
@@ -15,24 +18,26 @@ class Thread {
     }
 
     calculateWave() {
+        const p5 = this.p5;
         this.theta += this.angularVelocity;
 
         let x = this.theta;
         for (let i = 0; i < this.yvalues.length; i++) {
-            this.yvalues[i] = sin(x) * this.amplitude;
+            this.yvalues[i] = p5.sin(x) * this.amplitude;
             x += this.dx;
         }
     }
 
     render() {
+        const p5 = this.p5;
         this.calculateWave();
-        noStroke();
-        fill(this.color);
+        p5.noStroke();
+        p5.fill(this.color);
         this.circles = new Array();
         for (let x = 0; x < this.yvalues.length; x++) {
             let cx = x * this.xspacing;
             let cy = this.yaxis + this.yvalues[x];
-            ellipse(cx, cy, this.radius, this.radius);
+            p5.ellipse(cx, cy, this.radius, this.radius);
             this.circles.push({
                 x: cx,
                 y: cy,
@@ -41,9 +46,10 @@ class Thread {
     }
 
     clicked(x, y) {
+        const p5 = this.p5;
         for (let i = 0; i < this.circles.length; i++) {
             const circle = this.circles[i];
-            let d = dist(x, y, circle.x, circle.y);
+            let d = p5.dist(x, y, circle.x, circle.y);
             if (d < this.radius) {
                 console.log('ohohohohoh');
             }
@@ -51,12 +57,15 @@ class Thread {
     }
 }
 
-const colorScale = d3.scaleSequential(d3.interpolatePlasma).domain([0,1]);
-let threads = new Object();
-const userId = '2cb0e03eef321c467dfa07b70bda2fdada09696253cc5f9d590753bf1aa9dc1f';
-function setup() {
-    createCanvas(windowWidth, windowHeight);
-    background(0);
+const sketch = function(p5) {
+    const colorScale = d3.scaleSequential(d3.interpolatePlasma).domain([0,1]);
+    let threads = new Object();
+    const userId = '2cb0e03eef321c467dfa07b70bda2fdada09696253cc5f9d590753bf1aa9dc1f';
+
+  p5.setup = function() {
+    p5.createCanvas(900, 900)
+    p5.createCanvas(p5.windowWidth, p5.windowHeight);
+    p5.background(0);
     getBookDatas(userId)
       .then((bookDatas) => {
           const bookDatasArray = Object.entries(bookDatas);
@@ -72,28 +81,71 @@ function setup() {
                   color: colorScale(0.5),
                   yaxis: 250 * i + 150,
               }
-              console.log(param.height)
-              const thread = new Thread(param);
+              const thread = new Thread(p5, param);
               threads[i] = thread
           }
       });
-}
+  }
 
-function draw() {
-    background(0);
+  p5.draw = function() {
+    p5.background(0);
     for(let i in threads) {
         threads[i].render()
     }
+  }
+
+  p5.windowResized = function() {
+    p5.resizeCanvas(p5.windowWidth, p5.windowHeight);
+    p5.background(0);
+  }
+
+  p5.mousePressed = function() {
+      thread.clicked(p5.mouseX, p5.mouseY);
+  }
 }
 
-function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
-    background(0);
-}
+new P5(sketch)
 
-function mousePressed() {
-    thread.clicked(mouseX, mouseY);
-}
+//function setup() {
+    //createCanvas(windowWidth, windowHeight);
+    //background(0);
+    //getBookDatas(userId)
+      //.then((bookDatas) => {
+          //const bookDatasArray = Object.entries(bookDatas);
+          //for (let i = 0; i < bookDatasArray.length; i++) {
+              //const [ title ] = bookDatasArray[i];
+              //const param = {
+                  //title: title,
+                  //xspacing: 7,
+                  //theta: 0,
+                  //angularVelocity: 0.04,
+                  //amplitude: 75.0,
+                  //period: 200,
+                  //color: colorScale(0.5),
+                  //yaxis: 250 * i + 150,
+              //}
+              //console.log(param.height)
+              //const thread = new Thread(param);
+              //threads[i] = thread
+          //}
+      //});
+//}
+
+//function draw() {
+    //background(0);
+    //for(let i in threads) {
+        //threads[i].render()
+    //}
+//}
+
+//function windowResized() {
+    //resizeCanvas(windowWidth, windowHeight);
+    //background(0);
+//}
+
+//function mousePressed() {
+    //thread.clicked(mouseX, mouseY);
+//}
 
 function getBookDatas(userId) {
     const userRef = firebase.firestore().collection('users').doc(userId);
