d9110722c5394c640883899f96292abcfcefc606
Seita Uchimura <c201552k@gmail.com>
2019-02-26 14:02:44 -0800

    :+1: Update:.babelrcの設定を更新

--- src/js/main.js
+++ src/js/main.js
@@ -57,6 +57,40 @@ class Thread {
     }
 }
 
+function getBookDatas(userId) {
+    const userRef = firebase.firestore().collection('users').doc(userId);
+    const booksRef = userRef.collection('books');
+    return booksRef.get()
+        .then((books) => {
+            let titles = new Array();
+            books.forEach((book) => {
+                const title = book.data().title;
+                titles.push(title);
+            });
+
+            let bookDatas = new Object();
+            let promiseChain = Promise.resolve(bookDatas);
+            //for (let i = 0; i < titles.length; i++) {
+            for (let i = 0; i < 5; i++) {
+                promiseChain = promiseChain.then((bookDatas) => {
+                    return booksRef.doc(titles[i]).collection('lines').get()
+                        .then((lines) => {
+                            bookDatas[titles[i]] = new Array();
+                            lines.forEach((line) => {
+                                bookDatas[titles[i]].push(line.data().line);
+                            });
+                            return Promise.resolve(bookDatas);
+                        })
+                        .catch((err) => {
+                            console.error(err);
+                        });
+                });
+            }
+            return promiseChain;
+        })
+        .catch((err) => console.error(err));
+}
+
 const sketch = function(p5) {
     const colorScale = d3.scaleSequential(d3.interpolatePlasma).domain([0,1]);
     let threads = new Object();
@@ -106,77 +140,3 @@ const sketch = function(p5) {
 
 new P5(sketch)
 
-//function setup() {
-    //createCanvas(windowWidth, windowHeight);
-    //background(0);
-    //getBookDatas(userId)
-      //.then((bookDatas) => {
-          //const bookDatasArray = Object.entries(bookDatas);
-          //for (let i = 0; i < bookDatasArray.length; i++) {
-              //const [ title ] = bookDatasArray[i];
-              //const param = {
-                  //title: title,
-                  //xspacing: 7,
-                  //theta: 0,
-                  //angularVelocity: 0.04,
-                  //amplitude: 75.0,
-                  //period: 200,
-                  //color: colorScale(0.5),
-                  //yaxis: 250 * i + 150,
-              //}
-              //console.log(param.height)
-              //const thread = new Thread(param);
-              //threads[i] = thread
-          //}
-      //});
-//}
-
-//function draw() {
-    //background(0);
-    //for(let i in threads) {
-        //threads[i].render()
-    //}
-//}
-
-//function windowResized() {
-    //resizeCanvas(windowWidth, windowHeight);
-    //background(0);
-//}
-
-//function mousePressed() {
-    //thread.clicked(mouseX, mouseY);
-//}
-
-function getBookDatas(userId) {
-    const userRef = firebase.firestore().collection('users').doc(userId);
-    const booksRef = userRef.collection('books');
-    return booksRef.get()
-        .then((books) => {
-            let titles = new Array();
-            books.forEach((book) => {
-                const title = book.data().title;
-                titles.push(title);
-            });
-
-            let bookDatas = new Object();
-            let promiseChain = Promise.resolve(bookDatas);
-            //for (let i = 0; i < titles.length; i++) {
-            for (let i = 0; i < 5; i++) {
-                promiseChain = promiseChain.then((bookDatas) => {
-                    return booksRef.doc(titles[i]).collection('lines').get()
-                        .then((lines) => {
-                            bookDatas[titles[i]] = new Array();
-                            lines.forEach((line) => {
-                                bookDatas[titles[i]].push(line.data().line);
-                            });
-                            return Promise.resolve(bookDatas);
-                        })
-                        .catch((err) => {
-                            console.error(err);
-                        });
-                });
-            }
-            return promiseChain;
-        })
-        .catch((err) => console.error(err));
-}
