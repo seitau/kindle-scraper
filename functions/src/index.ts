1715791edfddf827a4903d97e276c9522d5014cb
Seita Uchimura <c201552k@gmail.com>
2019-03-15 18:29:58 -0700

    :wastebasket: Remove:ogp function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,8 @@
 //import evernoteAuth from './functions/evernoteAuth';
-//import ogp from './functions/ogp';
 import scrapeKindle from './functions/scrapeKindle';
 import analyzeLanguage from './functions/analyzeLanguage';
 
 //export const EvernoteAuth = evernoteAuth;
-//export const Ogp = ogp;
 export const ScrapeKindle = scrapeKindle;
 export const AnalyzeLanguage = analyzeLanguage;
 
