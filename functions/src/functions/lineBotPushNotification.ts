a10d41c455f8f6e87d7a63bda01b9c59d50a95a5
Seita Uchimura <c201552k@gmail.com>
2019-05-06 21:18:51 -0700

    :wrench: Fix:lineBotPushNotificationを修正

--- functions/src/functions/lineBotPushNotification.ts
+++ functions/src/functions/lineBotPushNotification.ts
@@ -20,8 +20,38 @@ const groupId = "Rbc2f4675368f47385fb4663a8129076e";
 
 export default firebase.functions
     .https.onRequest(async (req, res) => {
-        console.log(req.params.pusher);
-        const message = "hogejoge";
+        if (Object.keys(req.body).length === 0) {
+            return res.status(200).send(`Success: empty request`)
+        }
+        console.log(req.body);
+
+        const commitMessage = req.body.head_commit.message;
+        const pusher = req.body.pusher.name;
+        let commiter = '無名';
+        if (pusher === 'seita-uc') {
+            commiter = 'イケイケエンジニア様';
+        } else if (pusher === 'Noiseshunk') {
+            commiter = 'ズル剥けコンサルタント';
+        } else if (pusher === 'knose24') {
+            commiter = 'イキリ帰国子女';
+        } 
+
+        let additionalMessage;
+        const num = Date.now() % 3;
+        console.log(num);
+        switch(num) {
+            case 0:
+                additionalMessage = "もっともっと頑張ってね！❤️";
+                break;
+            case 1:
+                additionalMessage = "このくらいで満足するなよ？❤️";
+                break;
+            case 2:
+                additionalMessage = `${commiter}大好き！！！！`;
+                break;
+        }
+
+        const message = `${commiter}が${commitMessage}をコミットしてくれたみたい！${additionalMessage}`;
         return client.pushMessage(groupId, {
             type: 'text',
             text: message,
