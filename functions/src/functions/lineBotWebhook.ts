e577cb0e9e597901982feb5b7601697b9382aa27
Seita Uchimura <c201552k@gmail.com>
2019-05-13 13:07:53 -0700

    :watermelon: Add:lineBotWebhookにrecruitのapiを追加

--- functions/src/functions/lineBotWebhook.ts
+++ functions/src/functions/lineBotWebhook.ts
@@ -1,18 +1,22 @@
 import firebase from '../common/firebase';
 import * as line from '@line/bot-sdk';
+import rp from 'request-promise';
 
 let config;
+let recruitApiKey;
 if (process.env.NODE_ENV === 'test') {
     config = {
         channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
         channelSecret: process.env.LINE_CHANNEL_SECRET,
     };
+    recruitApiKey = process.env.RECRUIT_API_KEY;
 } else {
     const channel_env = firebase.functions.config().channel;
     config = {
         channelAccessToken: channel_env.access_token,
         channelSecret: channel_env.secret,
     };
+    recruitApiKey = firebase.functions.config().recruit.apikey;
 }
 
 const client = new line.Client(config);
@@ -22,24 +26,31 @@ function handleEvent(event) {
   let message;
 
   if (event.type === 'message' && event.message.type === 'text') {
-      //message = event.message.text;
+      message = event.message.text;
       const userId = event.source.userId;
-      return client.getProfile(userId)
-          .then((profile) => {
-              console.log(userId);
-              console.log(profile.displayName);
-              console.log(profile.pictureUrl);
-              console.log(profile.statusMessage);
-              return firebase.db
-                  .collection('line_users')
-                  .doc(userId)
-                  .set({
-                      name: profile.displayName,
-                  })
-          })
-          .catch((err) => {
-              console.error(err);
-          });
+      if(message.includes("かんなちゃん") || message.includes("カンナちゃん")) {
+          const options = {
+              method: 'POST',
+              uri: 'https://api.a3rt.recruit-tech.co.jp/talk/v1/smalltalk',
+              form: {
+                  apikey: recruitApiKey,
+                  query: message
+              },
+          };
+          return rp(options)
+              .then((response) => {
+                  const res = JSON.parse(response);
+                  console.log(res.results[0].reply)
+                  return client.replyMessage(event.replyToken, { 
+                      type: "text", 
+                      text: res.results[0].reply
+                  });
+              })
+              .catch((err) => {
+                  return console.error(err)
+              });
+      }
+      return
   } 
 
   if (event.type === 'join') {
